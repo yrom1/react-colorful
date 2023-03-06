b57608ae5e23d3f4af85371d5a36d5820a03e2bf
Ryan Moore <ryanm.inbox@gmail.com>
2023-03-06 01:13:48 -0500

    cycle though the colors i liked

--- colorful/src/App.js
+++ colorful/src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // #DCB0CF orangy-pink
-  // #9CF8EF light funky green
-  // #C773AD colar pink
-  // #E4D5FA pinkish white
-  // #90E8FA electric light greenblue
-  // #E7F9D0 light limey yellow
+  const colors = [
+    '#DCB0CF', // orangy-pink
+    '#9CF8EF', // light funky green
+    '#E4D5FA', // pinkish white
+    '#90E8FA', // electric light greenblue
+    '#E7F9D0'  // light limey yellow
+  ];
+
   const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    return colors[Math.floor(Math.random() * colors.length)];
   }
 
   const [color, setColor] = useState(getRandomColor());
