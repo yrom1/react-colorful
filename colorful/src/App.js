519d19ccc72528f2597bdb5bfd570c09184d8ed6
Ryan Moore <ryanm.inbox@gmail.com>
2023-03-06 01:33:29 -0500

    <ColorSection/> autogenerated

--- colorful/src/App.js
+++ colorful/src/App.js
@@ -28,30 +28,45 @@ function App() {
     '#E7F9D0'  // light limey yellow
   ];
 
-  const [color1, setColor1] = useState(colors[0]);
-  const [color2, setColor2] = useState(colors[1]);
+  const getRandomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  const [color1, setColor1] = useState(getRandomColor());
+  const [color2, setColor2] = useState(getRandomColor());
+
+  const sections = [
+    {
+      color: color1,
+      isFullScreen: true,
+      header: "Yo I'm Ryan",
+      text: "This is the first section. It takes up the whole height of the viewport."
+    },
+    {
+      color: color2,
+      isFullScreen: false,
+      header: "I'm sick af",
+      text: "This is the second section. It adjusts to the size of the text."
+    }
+    // Add more objects with different props here to add more sections
+  ];
 
   const handleClick = () => {
-    const randomIndex1 = Math.floor(Math.random() * colors.length);
-    const randomIndex2 = Math.floor(Math.random() * colors.length);
-    setColor1(colors[randomIndex1]);
-    setColor2(colors[randomIndex2]);
+    setColor1(getRandomColor());
+    setColor2(getRandomColor());
   }
 
   return (
     <div onClick={handleClick}>
-      <ColorSection
-        color={color1}
-        isFullScreen={true}
-        header="Section 1 Header"
-        text="This is the first section. It takes up the whole height of the viewport."
-      />
-      <ColorSection
-        color={color2}
-        isFullScreen={false}
-        header="Section 2 Header"
-        text="This is the second section. It adjusts to the size of the text."
-      />
+      {sections.map((section, index) => (
+        <ColorSection
+          key={index}
+          color={section.color}
+          isFullScreen={section.isFullScreen}
+          header={section.header}
+          text={section.text}
+        />
+      ))}
     </div>
   );
 }
