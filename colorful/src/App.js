9ed1f3763a7ff6037faec77d82f1bb759d1824c0
Ryan Moore <ryanm.inbox@gmail.com>
2023-03-08 20:02:04 -0500

    convert from bad sections thing to list of components

--- colorful/src/App.js
+++ colorful/src/App.js
@@ -3,24 +3,16 @@ import './App.css';
 import { debounce } from 'lodash';
 import { useState, useEffect } from 'react';
 
-function ColorSection({ color, isFullScreen, header, text, image, random }) {
-  const height = isFullScreen ? '100vh' : 'auto';
+function ColorSection({ color, content }) {
   const style = {
-    backgroundColor: color,
-    height: height,
+    color: color,
+    borderLeft: `2px solid ${color}`,
     padding: '20px',
-    borderRadius: random ? `${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% / ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}%` : null
+    textDecoration: 'underline',
   };
-  const imgStyle = {
-    maxWidth: '25%',
-    height: 'auto'
-  };
-
   return (
     <div style={style}>
-      <h1>{header}</h1>
-      {image && <img src={image} alt="" style={imgStyle} />}
-      <p>{text}</p>
+      {content}
     </div>
   );
 }
@@ -34,15 +26,14 @@ function generateRandomColors(len, getRandomColor) {
     colors.push(color);
   }
 
-  if (colors.length > 1) {
-    // Keep getting a new color and making it the last element of the array
-    while (colors[0] === colors[colors.length - 1]) {
-      let color = getRandomColor(lastColor);
-      lastColor = color;
-      colors[colors.length - 1] = color;
-    }
-  }
-
+  // if (colors.length > 1) {
+  //   // Keep getting a new color and making it the last element of the array
+  //   while (colors[0] === colors[colors.length - 1]) {
+  //     let color = getRandomColor(lastColor);
+  //     lastColor = color;
+  //     colors[colors.length - 1] = color;
+  //   }
+  // }
   return colors;
 }
 
@@ -65,35 +56,38 @@ function App() {
   }
 
   const sections = [
-    {
-      header: "Yo I'm Ryan",
-      text: "This is the first section. It takes up the whole height of the viewport.",
-      isFullScreen: true
-    },
-    {
-      header: "I'm sick af",
-      text: "This is the second section. It adjusts to the size of the text.",
-      isFullScreen: false,
-      image: "https://upload.wikimedia.org/wikipedia/commons/8/87/Vincent_van_Gogh_-_Head_of_a_skeleton_with_a_burning_cigarette_-_Google_Art_Project.jpg"
-    },
-    {
-      header: "Making stuff is fun",
-      text: "This is the third section. It adjusts to the size of the text.",
-      isFullScreen: false,
-    },
-    {
-      header: "I like colors",
-      text: "This is the third section. It adjusts to the size of the text.",
-      isFullScreen: false
-    },
-    {
-      header: "And websites",
-      text: "This is the fourth section. It adjusts to the size of the text.",
-      isFullScreen: false
-    },
+    <>
+      <h1>Yo I'm Ryan</h1>
+      <p>This is the first section. It takes up the whole height of the viewport.</p>
+    </>,
+    <>
+      <h2>I'm sick af</h2>
+      <p>This is the second section. It adjusts to the size of the text.</p>
+      <img
+        style={{
+          maxWidth: '25%',
+          height: 'auto'
+        }}
+        src="https://upload.wikimedia.org/wikipedia/commons/8/87/Vincent_van_Gogh_-_Head_of_a_skeleton_with_a_burning_cigarette_-_Google_Art_Project.jpg"
+        alt="Vincent van Gogh - Head of a skeleton with a burning cigarette"
+      />
+    </>,
+    <>
+      <h2>Making stuff is fun</h2>
+      <p>This is the third section. It adjusts to the size of the text.</p>
+    </>,
+    <>
+      <h2>I like colors</h2>
+      <p>This is the fourth section. It adjusts to the size of the text.</p>
+    </>,
+    <>
+      <h2>And websites</h2>
+      <p>This is the fifth section. It adjusts to the size of the text.</p>
+    </>,
   ];
 
-  const numComponents = sections.length + 1; // headerbar
+
+  const numComponents = sections.length + 2; // headerbar + footerbar
   const [colorsState, setColorsState] = useState(() => generateRandomColors(numComponents, getRandomColor));
 
   const handleScroll = debounce(() => {
@@ -161,23 +155,25 @@ function App() {
       <p></p>
       <p></p>
       <p></p>
-      <p><i>Thanks for visiting</i></p>
+      <p><i><b>Thanks for visiting</b></i></p>
       <p></p>
+    </div>
+  )
+  const foo = (
+    <div>
+      <p>Hewwo</p>
     </div>
   )
 
   return (
-    <div onClick={handleClick} style={{ backgroundColor: 'white' }}>
+    <div onClick={handleClick} style={{ backgroundColor: '#1F2225' }}>
       <Bar color={colorsState[0]} content={header_content} />
-      {sections.map((section, index) => (
+      {/* <div style={{ paddingTop: '100vh' }}><p>Hi</p></div> */}
+      {sections.map((_, index) => (
         <ColorSection
           key={index}
           color={colorsState[index + 1]} // skip the color of the header bar
-          isFullScreen={section.isFullScreen}
-          header={section.header}
-          text={section.text}
-          image={section.image}
-          random={index === 0 ? true : false}
+          content={_}
         />
       ))}
       <Bar color={colorsState[0]} content={footer_content} />
